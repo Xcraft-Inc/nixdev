8d6afcf359c89df9def82f494e4fe190c3525cd3
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-26 14:08:14 +0200

    Implement the functions in order to call the scripts of each package.

--- pkg/pkg.sh
+++ pkg/pkg.sh
@@ -1,36 +1,53 @@
 #!/bin/sh
 
+. ./pkg/config
+
 function pkg_uninstall()
 {
   local package=$1
 
+  . ./packages/$package/config
+
   echo "[stage0:$package] uninstall"
+  . ./packages/$package/uninstall.sh
 }
 
 function pkg_install()
 {
   local package=$1
 
+  . ./packages/$package/config
+
   echo "[stage0:$package] install"
+  . ./packages/$package/install.sh
 }
 
 function pkg_remove()
 {
   local package=$1
 
+  . ./packages/$package/config
+
   echo "[stage0:$package] remove"
+  . ./packages/$package/remove.sh
 }
 
 function pkg_get()
 {
   local package=$1
 
+  . ./packages/$package/config
+
   echo "[stage0:$package] get"
+  . ./packages/$package/get.sh
 }
 
 function pkg_build()
 {
   local package=$1
 
+  . ./packages/$package/config
+
   echo "[stage0:$package] build"
+  . ./packages/$package/build.sh
 }
