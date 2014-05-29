83a139843a667d38bd0bb7eb8350b03a8f522c92
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-29 10:52:13 +0200

    When a runner is trying to work with the bootstrapper, it must use the gitlab-ci-token instead of asking for Git username and password.

--- packages/toolchain/get.sh
+++ packages/toolchain/get.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Enter your git.epsitec.ch username:"
-read username
-
 git config --global http.sslVerify false
+
+location=
+
+if [ "$USER" != "gitlab_ci_runner" ]; then
+  echo "Enter your git.epsitec.ch username:"
+  read username
+  location=$username
+else
+  location=gitlab-ci-token:$pkg_ci_token
+fi
 
 git clone "https://$username@$pkg_src" "$pkg_dst"
