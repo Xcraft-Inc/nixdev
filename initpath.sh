c00d6a8dce9e6e4b5cbb6d128f89c768444e6e7e
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-23 21:30:46 +0200

    Ask to add a symlink to node in the ~/bin directory.

--- initpath.sh
+++ initpath.sh
@@ -9,4 +9,24 @@ function pathadd()
   fi
 }
 
-pathadd "$boot_sysroot/bin"
+# Try to add our node in the user's bin directory.
+echo "It is recommended to add a node symlink to your ~/bin directory; do you agree? [yN]"
+read input
+
+if [ "$input" != y ]; then
+  pathadd "$boot_sysroot/bin"
+  exit 0
+fi
+
+if [ ! -d "$HOME/bin" ]; then
+  mkdir "$HOME/bin"
+else
+  if [ -f "$HOME/bin/node" ]; then
+    echo "A node binary already exists in your ~/bin directory; overwrite? [yN]"
+    read input
+    [ "$input" != y ] && exit 0
+    rm -f "$HOME/bin/node"
+  fi
+fi
+
+ln -s "$(pwd)/sysroot/bin/node" "$HOME/bin/node"
