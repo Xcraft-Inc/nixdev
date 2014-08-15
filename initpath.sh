a0f3df0e24936ea4768f2b4040d60aab257d336c
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-15 11:41:22 +0200

    No more symlink for node in ~/bin.

--- initpath.sh
+++ initpath.sh
@@ -9,30 +9,4 @@ function pathadd()
   fi
 }
 
-[ "$1" = quiet ] && quiet=y
-
-# Try to add our node in the user's bin directory.
-if [ "$quiet" != y ]; then
-  echo "It is recommended to add a node symlink to your ~/bin directory; do you agree? [yN]"
-  read input
-
-  if [ "$input" != y ]; then
-    pathadd "$boot_sysroot/bin"
-    exit 0
-  fi
-fi
-
-if [ ! -d "$HOME/bin" ]; then
-  mkdir "$HOME/bin"
-else
-  if [ "$quiet" != y -a -f "$HOME/bin/node" ]; then
-    echo "A node binary already exists in your ~/bin directory; overwrite? [yN]"
-    read input
-    [ "$input" != y ] && exit 0
-  fi
-  rm -f "$HOME/bin/node"
-  rm -f "$HOME/bin/npm"
-fi
-
-ln -s "$(pwd)/sysroot/bin/node" "$HOME/bin/node"
-ln -s "$(pwd)/sysroot/bin/npm" "$HOME/bin/npm"
+pathadd "$boot_sysroot/bin"
