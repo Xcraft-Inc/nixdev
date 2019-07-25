2ec7a4daf3e1159e28e07944bc648c8537b6502a
Mathieu Schroeter <schroeter@epsitec.ch>
2019-07-25 13:23:02 +0200

    Optimize the nodejs binary

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export CFLAGS="-O2 -g0 -mtune=native" CXXFLAGS="-O2 -g0 -mtune=native"
+
 cd "$boot_build/nodejs/"
 ./configure --prefix="$boot_sysroot/usr"
 make -j$boot_nproc
 cd -
+
+unset CFLAGS CXXFLAGS
