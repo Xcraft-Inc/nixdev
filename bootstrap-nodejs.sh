8f93f3cfe39ac69c44cffe3326c9bc45f70508b2
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-06-09 08:25:17 +0200

    Rename toolchain to xcraft

--- bootstrap-nodejs.sh
+++ bootstrap-nodejs.sh
@@ -24,23 +24,23 @@ function boot_normal()
   pkg_build nodejs
   pkg_install nodejs
 
-  pkg_get toolchain
+  pkg_get xcraft
 
   unset CFLAGS CXXFLAGS
 
   echo "[stage0] end of stage0"
 
   . ./initpath.sh $quiet
-  node "$boot_sysroot/toolchain/lib/xcraft/stage1.js" "$boot_sysroot/usr/bin"
+  node "$boot_sysroot/xcraft/lib/xcraft/stage1.js" "$boot_sysroot/usr/bin"
 
-  cd "$boot_sysroot/toolchain/" || return
+  cd "$boot_sysroot/xcraft/" || return
 }
 
 function boot_dist()
 {
   echo "Building a dist package"
   pkg_cache nodejs
-  pkg_cache toolchain
+  pkg_cache xcraft
 }
 
 if [ $# = 1 ] && [ "$1" = dist ]; then
