c835a8d125cc707b3fcd15fa3d9b6c8a15d2ad65
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-09-28 10:21:12 +0200

    Specify the npm version explicitly

--- packages/nodejs/install.sh
+++ packages/nodejs/install.sh
@@ -4,4 +4,4 @@ cd "$pkg_dst" || exit 1
 make install
 cd - || exit 1
 
-"$boot_sysroot/usr/bin/npm" install --prefix="$boot_sysroot/usr/lib" npm@7
+"$boot_sysroot/usr/bin/npm" install --prefix="$boot_sysroot/usr/lib" npm@7.24.1
