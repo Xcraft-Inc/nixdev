7859ce4c1a0bba8228b5504958c1b6c3633b5888
Mathieu Schroeter <schroeter@epsitec.ch>
2022-06-21 15:11:08 +0200

    Fix zlib linking with Monterey

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd "$boot_build/nodejs/" || exit 1
-./configure --prefix="$boot_sysroot/usr"
+
+args=""
+# Use system zlib with macOS Monterey
+if [[ $(uname -r) =~ ^21\. ]] && [[ $(uname -s) = Darwin ]]; then
+  args="--shared-zlib"
+fi
+
+./configure --prefix="$boot_sysroot/usr" $args
 make -j"$boot_nproc"
 cd - || exit 1
