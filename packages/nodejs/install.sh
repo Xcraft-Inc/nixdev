55a368742e09c9880aad9190c4afc8eaf1b7a46f
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-03-24 08:13:12 +0100

    Upgrade node and npm

--- packages/nodejs/install.sh
+++ packages/nodejs/install.sh
@@ -4,4 +4,4 @@ cd "$pkg_dst" || exit 1
 make install
 cd - || exit 1
 
-"$boot_sysroot/usr/bin/node" "$boot_sysroot/usr/bin/npm" install --prefix="$boot_sysroot/usr/lib" npm@8.12.2
+"$boot_sysroot/usr/bin/node" "$boot_sysroot/usr/bin/npm" install --prefix="$boot_sysroot/usr/lib" npm@8.19.4
