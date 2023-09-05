42d9b14afe050a194414f3b1189a25dfcc37bb8e
Mathieu Schroeter <schroeter@epsitec.ch>
2023-09-05 08:52:12 +0000

    Upgrade npm to v9

--- packages/nodejs/install.sh
+++ packages/nodejs/install.sh
@@ -4,4 +4,4 @@ cd "$pkg_dst" || exit 1
 make install
 cd - || exit 1
 
-"$boot_sysroot/usr/bin/node" "$boot_sysroot/usr/bin/npm" install --prefix="$boot_sysroot/usr/lib" npm@8.19.4
+"$boot_sysroot/usr/bin/node" "$boot_sysroot/usr/bin/npm" install --prefix="$boot_sysroot/usr/lib" npm@9.8.1
