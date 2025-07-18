7678f70d27933c39658dd0f0e8159fd3ed337bc1
Mathieu Schroeter <mathieu@schroetersa.ch>
2025-07-18 09:03:31 +0200

    Ask for Xcraft Git host and path (use github by default)

--- packages/xcraft/get.sh
+++ packages/xcraft/get.sh
@@ -8,6 +8,27 @@ else
   origin=$(git remote get-url origin)
   protocol=${origin%:*}
 
+  if [ -z "$pkg_host" ]; then
+    echo "Choose a Git host for Xcraft (default: github.com)"
+    echo "For Epsitec SA, provide git.epsitec.ch"
+    read -r pkg_host
+    if [ -z "$pkg_host" ]; then
+      pkg_host="github.com"
+      pkg_path="Xcraft-Inc/xcraft.git"
+    fi
+  fi
+
+  if [ -z "$pkg_path" ]; then
+    echo "Choose a Git path for Xcraft (default: Xcraft-Inc/xcraft.git)"
+    echo "For Epsitec SA, provide xcraft/xcraft.git"
+    read -r pkg_path
+    if [ -z "$pkg_path" ]; then
+      pkg_path="Xcraft-Inc/xcraft.git"
+    fi
+  fi
+
+  pkg_src="$pkg_host/$pkg_path"
+
   if [ "$protocol" = https ] || [ "$protocol" = http ]; then
     echo "Enter your $pkg_host login:"
     read -r login
